aef35cd4c3b0aa91f03684f9c935ba6b84f38366
Omar-Al-Hajjeh <omarbilal9000@gmail.com>
2025-05-25 06:11:58 +0300

    Signed-off-by: Omar-Al-Hajjeh <omarbilal9000@gmail.com>

--- code froces/CF490-D2-A.cpp
+++ code froces/CF490-D2-A.cpp
@@ -1,12 +1,28 @@
-#include<iostream>
+#include <iostream>
+#include <vector>
 int main()
 {
-    int n,t;
-    std:: cin >> n;
-    int arr[n];
-    for(int i = 0; i < n ;i++){
-        std:: cin >> t;
-        arr[i] = t;
+    int n, t, i, result;
+    std::cin >> n;
+    std::vector<int> pro;
+    std::vector<int> math;
+    std::vector<int> ecx;
+
+    for (int i = 0; i < n; i++)
+    {
+        std::cin >> t;
+        if (t == 1)
+            pro.push_back(i + 1);
+        if (t == 2)
+            math.push_back(i + 1);
+        if (t == 3)
+            ecx.push_back(i + 1);
+    }
+    result = std::min(pro.size(),std::min(math.size(), ecx.size()));
+    std::cout << result << std::endl;
+    for (int i = 0; i < result; i++)
+    {
+        std::cout << pro[i] << " " << math[i] << " " << ecx[i] << std::endl;
     }
 
     return 0;
